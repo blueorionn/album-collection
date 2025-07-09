80ae2198ee5f686e704193293b3204d64b656171
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-07-09 05:01:04 +0000

    fix: removed signup option

--- views/layout.php
+++ views/layout.php
@@ -19,7 +19,6 @@
                 </a>
                 <div class="flex items-center lg:order-2">
                     <a href="/auth/login" class="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Log in</a>
-                    <a href="/auth/signup" class="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Sign up</a>
                 </div>
             </div>
         </nav>
