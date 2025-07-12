f99fd7e76d08fc265f689282da11931e41150df7
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-07-12 09:50:58 +0000

    fix: removed login username display section

--- views/layout.php
+++ views/layout.php
@@ -21,7 +21,6 @@
                     <?php if (!isset($_SESSION['user'])): ?>
                         <a href="/auth/login" class="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Log in</a>
                     <?php endif; ?>
-                    <span><?= $_SESSION['user'] ?></span>
                 </div>
             </div>
         </nav>
